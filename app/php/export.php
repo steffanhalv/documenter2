608b845b2df49f3ce9e0f210c51a5df10b066e42
Steffan Halvorsen <steffanhalv@gmail.com>
2015-06-30 14:11:44 +0200

    Added export functionality

--- app/php/export.php
+++ app/php/export.php
@@ -4,15 +4,53 @@ error_reporting(0);
 
 $json_output = json_decode(file_get_contents("php://input"), true);
 
+$newContent = '';
+
+$header = fopen("header.html", "r");
+// Output one line until end-of-file
+while(!feof($header)) {
+    $newContent = $newContent.fgets($header);
+}
+fclose($header);
 
 foreach ($json_output as $json){
 
     foreach ($json['sections'] as $section){
 
-        echo $section['model'];
+        $newContent = $newContent.$section['model'];
 
     }
 
 }
+
+$footer = fopen("footer.html", "r");
+// Output one line until end-of-file
+while(!feof($footer)) {
+    $newContent = $newContent.fgets($footer);
+}
+fclose($footer);
+
+$myfile = fopen("newfile.html", "w");
+fwrite($myfile, $newContent);
+fclose($myfile);
+
+$files = array(
+    'newfile.html'
+);
+
+
+$zip = new ZipArchive();
+$zip_name = "zipfile.zip";
+if($zip->open($zip_name, ZIPARCHIVE::CREATE)!==TRUE){
+    $error .= "* Sorry ZIP creation failed at this time";
+}
+
+foreach($files as $file){
+    $zip->addFile($file);
+}
+
+$zip->close();
+
+echo "zipfile.zip";
 
 ?>
